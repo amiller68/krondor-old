286988d099f64069eca6900ebbbccd88e053b9b0
amiller68 <alexanderscottmiller@gmail.com>
2021-12-27 20:16:29 -0500

    forgot thsese

---
+++ src/pages/front/front.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-front',
+  templateUrl: './front.component.html',
+  styleUrls: ['./front.component.sass']
+})
+export class FrontComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
