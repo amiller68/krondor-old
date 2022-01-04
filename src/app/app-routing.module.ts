6d61a9f3dfd109b7314e9530f5ed7c7053a1bdca
amiller68 <alexanderscottmiller@gmail.com>
2022-01-04 17:07:08 -0600

    hmm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FrontPageComponent } from './pages/front-page/front-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 
 const routes: Routes = [
-  { path: '', component: FrontPageComponent }
+  { path: '', component: FrontPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  // Wild card route
+  { path: '*', component: ErrorPageComponent }
 ];
 
 @NgModule({
