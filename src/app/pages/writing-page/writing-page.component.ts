8c99eb442ded8bc7702b9b149ffd7fa9e6a37456
amiller68 <alexanderscottmiller@gmail.com>
2022-01-05 19:33:30 -0600

    added a lot more stuff

---
+++ src/app/pages/writing-page/writing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-writing-page',
+  templateUrl: './writing-page.component.html',
+  styleUrls: ['./writing-page.component.sass']
+})
+export class WritingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/writing-page/writing-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WritingPageComponent } from './writing-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    WritingPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WritingPageModule { }
